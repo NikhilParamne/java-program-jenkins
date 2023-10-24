4372c3d5f4006e3f56ff17503c48851512acccdb
NikhilParamane <nikhilparamane@gmail.com>
2023-10-24 17:49:35 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Java program.");
+        System.out.println("Output from Java program.");
+        System.out.println(new Date());
+    }
+}
+
